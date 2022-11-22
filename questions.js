f04215c252a631d4a43a58e4c95a8a7e5376d55e
olidec <69003898+olidec@users.noreply.github.com>
2022-11-22 12:10:38 +0100

    qwerty

---
+++ questions.js
@@ -0,0 +1,23 @@
+data = [
+    '{"question": "The Plaka is the oldest quarter of which city?", "answers": {"a": "Athens", "b": "Prague", "c": "Rome", "d": "Vienna"}, "correctAnswer": "a"}',
+    '{"question": "What is an axolotl?", "answers": {"a": "A nerve in the brain", "b": "A multi-axled vehicle", "c": "A type of mortice lock", "d": "A species of salamander"}, "correctAnswer": "d"}',
+    '{"question": "The Panama Canal was officially opened by which US president?", "answers": {"a": "Calvin Coolidge", "b": "Herbert Hoover", "c": "Theodore Roosevelt", "d": "Woodrow Wilson"}, "correctAnswer": "d"}',
+    '{"question": "After Adam, Eve, Cain and Abel who is the next person mentioned in the Bible?", "answers": {"a": "Enoch", "b": "Jubal", "c": "Lamech", "d": "Zillah"}, "correctAnswer": "a"}',
+    '{"question": "Lisbon stands at the mouth of which river?", "answers": {"a": "Seine", "b": "Duoro", "c": "Rio Grande", "d": "Tagus"}, "correctAnswer": "d"}',
+    '{"question": "In which ocean is Madagascar?", "answers": {"a": "Atlantic", "b": "Pacific", "c": "Indian", "d": "Arctic"}, "correctAnswer": "c"}',
+    '{"question": "Which planet is said to influence the way we communicate?", "answers": {"a": "Venus", "b": "Mercury", "c": "Mars", "d": "Saturn"}, "correctAnswer": "b"}',
+    '{"question": "If you were born on New Years Day, which is your astrological sign?", "answers": {"a": "Aries", "b": "Cancer", "c": "Capricorn", "d": "Leo"}, "correctAnswer": "c"}',
+    '{"question": "Who was the last wife of Henry VIII?", "answers": {"a": "Anne of Cleves", "b": "Catherine of Aragon", "c": "Catherine Howard", "d": "Catherine Parr"}, "correctAnswer": "d"}',
+    '{"question": "What is the ruling planet of Scorpio?", "answers": {"a": "Pluto", "b": "Venus", "c": "Earth", "d": "Saturn"}, "correctAnswer": "a"}',
+    '{"question": "What country did Great Britain fight in the War of Jenkins Ear?", "answers": {"a": "France", "b": "Ireland", "c": "Spain", "d": "Wales"}, "correctAnswer": "c"}',
+    '{"question": "What colour is gin?", "answers": {"a": "Colourless", "b": "Blue", "c": "Green", "d": "Yellow"}, "correctAnswer": "a"}',
+    '{"question": "What is minestrone?", "answers": {"a": "Cheese", "b": "Noodles", "c": "A type of potato", "d": "Soup"}, "correctAnswer": "d"}',
+    '{"question": "With which baseball team did Jose DiMaggio achieve icon status in the 1940s?", "answers": {"a": "Chicago Bears", "b": "New York Yankees", "c": "Baltimore Batters", "d": "Cleveland Steelers"}, "correctAnswer": "b"}',
+    '{"question": "From which country does tennis player Andres Gomez, winner of the 1990 French Championships, come?", "answers": {"a": "Ecuador", "b": "Peru", "c": "Portugal", "d": "Spain"}, "correctAnswer": "a"}',
+    '{"question": "In which opera did Maria Callas make her last appearance at Covent Garden?", "answers": {"a": "Carmen", "b": "Tosca", "c": "Madame Butterfly", "d": "La Boheme"}, "correctAnswer": "b"}',
+    '{"question": "What is a kudzu?", "answers": {"a": "Antelope", "b": "Bird", "c": "Jewish settlement", "d": "Climbing plant"}, "correctAnswer": "a"}',
+    '{"question": "Which US city is located on the Maumee River at Lake Erie?", "answers": {"a": "Detroit", "b": "Toledo", "c": "Cleveland", "d": "Buffalo"}, "correctAnswer": "b"}',
+    '{"question": "What is the longest river in France?", "answers": {"a": "Loire", "b": "Seine", "c": "Rhone", "d": "Gironde"}, "correctAnswer": "a"}',
+    '{"question": "In which Australian state is Darwin?", "answers": {"a": "Queensland", "b": "Western Australia", "c": "Northern Territory", "d": "Tasmania"}, "correctAnswer": "c"}'
+  ];
+  
